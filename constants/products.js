f18ea7b78c06b04dd0f3b5ba2a258cc56ca49788
hugobsilva <hugobsilva@gmail.com>
2019-03-09 14:57:04 -0300

    Codigo Fonte Completo

---
+++ constants/products.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: 'Bombeiro Hidráulico',
+    image: 'https://source.unsplash.com/dS2hi__ZZMk/840x840',
+    price: 180,
+    horizontal: true,
+  },
+  {
+    title: 'Pedreiro',
+    image: 'https://source.unsplash.com/tb6ulgGY5Zc/840x840',
+    price: 220,
+  },
+  {
+    title: 'Eletricista',
+    image: 'https://source.unsplash.com/YHbcum51JB0/840x840',
+    price: 40,
+  },
+  {
+    title: 'Mecânico',
+    image: 'https://source.unsplash.com/I7BSOoPa5hM/840x840',
+    price: 188,
+    horizontal: true,
+  },
+  {
+    title: 'Baby Sitter',
+    image: 'https://source.unsplash.com/Ws4wd-vJ9M0/840x840',
+    price: 180,
+  },
+];
